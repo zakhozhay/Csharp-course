83cfd77ec4c0744e329da40cf0b4c89baac89d90
Oleh Zakhozhai <zakhozhay.oleg@gmail.com>
2021-03-29 00:12:37 +0300

    The new project`s tamplates was added

---
+++ Base Syntax/7 Arrays/7 Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Base Syntax/8 Functions/8 Functions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
